f81992029f03011a684cc1a22f1abe1740a5b5f1
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-11 16:15:42 +0200

    creation de une liste dans menu

---
+++ src/nav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Document</title>
+</head>
+<body>
+<span class="material-symbols-outlined">Person</span>
+<span class="material-symbols-outlined">Storefront</span>
+<span class="material-symbols-outlined">menu</span>
+<menu>
+    <ul>
+        <li>ROMANS</li>
+        <li>DB</li> 
+        <li>THEATRE</li>
+        <li>GRANDIR</li>
+        <li>ESSAIS</li>
+</ul>
+
+    <div>
+        <P>PAGE D'ACCUEIL</P>
+        <P>NOUVEAUTES</P>
+    </div>  
+</menu>  
+</body>
+</html>
