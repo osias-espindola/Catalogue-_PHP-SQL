535a0cbb85b489cdc2347bccd9ce12e7d10d63f0
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-11 16:56:56 +0200

    color du span

--- src/nav.php
+++ src/nav.php
@@ -4,25 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="style_nav.css">
     <title>Document</title>
 </head>
 <body>
+<nav>
 <span class="material-symbols-outlined">Person</span>
 <span class="material-symbols-outlined">Storefront</span>
 <span class="material-symbols-outlined">menu</span>
-<menu>
+
     <ul>
-        <li>ROMANS</li>
-        <li>DB</li> 
-        <li>THEATRE</li>
-        <li>GRANDIR</li>
-        <li>ESSAIS</li>
+        <li><a href="#">ROMANS</a></li>
+        <li><a href="#">DB</a></li> 
+        <li><a href="#">THEATRE</a></li>
+        <li><a href="#">GRANDIR</a></li>
+        <li><a href="#">ESSAIS</a></li>
 </ul>
 
     <div>
         <P>PAGE D'ACCUEIL</P>
         <P>NOUVEAUTES</P>
     </div>  
-</menu>  
+</nav>  
 </body>
 </html>
