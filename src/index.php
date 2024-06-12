b75a9620648c67aa8bd927cb3a236c8f7c330a92
PierreGauv <pierregauvin@hotmail.fr>
2024-06-12 11:05:08 +0200

    Page index

--- src/index.php
+++ src/index.php
@@ -4,19 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+           text-align: center;
+           color: white;
+        }
+
+        .header {
+            width: 100%;
+            height: 450px;
+            background-image: url("img/bibliotheque.png");
+            background-position: bottom;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
 </head>
 <body>
     <nav>
-        <p>test</p>
+        
     </nav>
     <section1>
+        <div class="header">
+            <h1>Bienvenue au coin de Littérature</h1>
 
+        </div>
     </section>
     <section2>
 
     </section>
     <footer>
-
+        <?php
+        include 'footer.php';
+        ?>
     </footer>
 </body>
 </html>
