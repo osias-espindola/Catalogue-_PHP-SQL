a09fa155736588d9d48349cb83e8967bfd1f35d9
PierreGauv <pierregauvin@hotmail.fr>
2024-06-18 05:41:04 +0200

    correction erreur dans index section 3

--- src/index.php
+++ src/index.php
@@ -238,7 +238,7 @@ foreach ($genres as $genre) {
                 <h2><?= $genre ?></h2>
                 <?php if (isset($livres_genre[$genre])): ?>
                     <div class="pad_carte">
-                        <a href="fiche_produit.php?id=<?= $livres_genre[$genre]["id"]?>"><img src="display_image.php?id=<?= $livres_genre[$genre]['titre'] ?>" alt="<?= $livres_genre[$genre]['auteur'] ?>"></a>
+                        <a href="fiche_produit.php?id=<?=$livres_genre[$genre]["id"]?>"><img src="display_image.php?id=<?=$livres_genre[$genre]['id'] ?>" alt="<?= $livres_genre[$genre]['auteur'] ?>"></a>
                     </div>
                 <?php endif; ?>
             </div>
