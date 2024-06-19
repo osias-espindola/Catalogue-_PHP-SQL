1abbee24ae3d7fa46b27e479d47a1eea83e6dfb2
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-19 13:49:20 +0200

    onclick pour fermer

--- src/nav.php
+++ src/nav.php
@@ -134,7 +134,7 @@ $tous_genre = $query->fetchAll(PDO::FETCH_ASSOC);
                 </li>
              </ul>
         </div> <!--class=menu_sous_genre-->
-        <p ><a href="#" class="bt_fermer"><img src="/img/fermer.png" alt="fermer"></a></p>
+        <p ><a href="#" class="bt_fermer"><img src="/img/fermer.png" alt="fermer"  onclick= "clickMenu()"></a></p>
     </div> <!--menu_ul-->
 </nav>  
 <script>
